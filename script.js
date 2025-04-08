fa058c82355bafa9de74447a48741a13f9c70cef
Gustavo Gutierrez <gustavo2023@gmail.com>
2025-04-08 15:02:09 -0400

    feat(form): implement form submission handling and email validation

---
+++ script.js
@@ -0,0 +1,30 @@
+const newsletterContainer = document.querySelector(".newsletter-container");
+const form = document.getElementById("form");
+const emailInput = document.getElementById("email");
+const errorMessage = document.querySelector(".error-message");
+const submitBtn = document.getElementById("submit");
+const successMessage = document.querySelector(".success-message");
+
+const handleFormSubmit = (e) => {
+  e.preventDefault(e);
+
+  const emailValue = emailInput.value.trim();
+  const error = validateEmail(emailValue);
+
+  if (error) {
+    errorMessage.textContent = error;
+    errorMessage.style.display = "flex";
+    return;
+  }
+};
+
+const validateEmail = (email) => {
+  if (!email) return "Email is required";
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) return "Valid email required";
+
+  return "";
+};
+
+form.addEventListener("submit", handleFormSubmit);
